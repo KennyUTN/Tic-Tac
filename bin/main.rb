ef36866754b022e5c46aa40af9c6c5b991b3eb1e
KennyUTN <asisignacio@gmail.com>
2021-03-15 16:52:56 -0300

    linter

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,7 @@
 puts 'Welcome to the Tic-Tac-Toe Experience \n'
 
 playing_game = true
-  i = 0
+
 while playing_game
 
   puts 'Player one please enter your name!, you will be X'
@@ -17,14 +17,13 @@ while playing_game
   puts "#{player1} VS #{player2}"
   puts '////////////////////////////'
 
-
   puts "#{player1} you are X"
   puts "#{player2} you are O"
 
   changing_turns = true
   while changing_turns
 
-    i =+ 1
+    i = + 1
     current_player = player1 if i.odd?
     current_player = player2 if i.even?
 
@@ -46,38 +45,38 @@ while playing_game
       move = gets.chomp.to_i
     end
 
-
     puts '(Test build: Press 3 to win and 4 to draw)'
 
     win = 3
     draw = 4
-
-    if move = win
+# rubocop: disable Metrics/BlockNesting
+    if move == win
       puts '- - - - - - - - - - - - - - - - - -'
       puts "Game finished, #{current_player}, You Won!"
       puts '- - - - - - - - - - - - - - - - - -'
       puts 'Do you want to replay? Y/N'
-     status = gets.chomp.upcase
-     while status != 'Y' && status != 'N'
-       puts 'Please type Y or N'
-       status = gets.chomp.upcase
-     end
-     playing_game = false if status == 'N'
-     changing_turns = false if status == 'Y'
-   end
-
-   if move = draw
-     puts '- - - - - - - - - - - - - - - - - -'
-     puts "Game finished, its a draw!"
-     puts '- - - - - - - - - - - - - - - - - -'
-     puts 'Do you want to replay? Y/N'
-    status = gets.chomp.upcase
-    while status != 'Y' && status != 'N'
-      puts 'Please type Y or N'
       status = gets.chomp.upcase
-    end
-    playing_game = false if status == 'N'
-    changing_turns = false if status == 'Y'
+      while status != 'Y' && status != 'N'
+        puts 'Please type Y or N'
+        status = gets.chomp.upcase
+      end
+      playing_game = false if status == 'N'
+      changing_turns = false if status == 'Y'
+
+    elsif move == draw
+
+      puts '- - - - - - - - - - - - - - - - - -'
+      puts 'Game finished, its a draw!'
+      puts '- - - - - - - - - - - - - - - - - -'
+      puts 'Do you want to replay? Y/N'
+      status = gets.chomp.upcase
+      while status != 'Y' && status != 'N'
+        puts 'Please type Y or N'
+        status = gets.chomp.upcase
+      end
+      playing_game = false if status == 'N'
+      changing_turns = false if status == 'Y'
+      # rubocop: enable Metrics/BlockNesting
     end
   end
 end
