67065f6de9b7491206baf7bdafda16f278c3f5a5
KennyUTN <asisignacio@gmail.com>
2021-03-16 15:11:28 -0300

    linters

--- bin/main.rb
+++ bin/main.rb
@@ -10,10 +10,9 @@ def initialize
   @player1 = Players.player1
   @player2 = Players.player2
   @current_player = @player1
-  turns = 0
 end
 
-puts 'Welcome to the Tic-Tac-Toe Experience \n'
+puts 'Welcome to the Tic-Tac-Toe '
 
 playing_game = true
 
@@ -35,24 +34,25 @@ while playing_game
   puts "#{player2} you are O"
 
   changing_turns = true
+  turns = 0
   while changing_turns
 
-    turns + = 1
-    current_player = player1 if i.odd?
-    current_player = player2 if i.even?
+    turns += 1
+    @current_player == @player1 if turns.odd?
 
+    @current_player == @player2 if turns.even?
 
     puts "#{current_player} it is your turn!"
     print "\n\n"
-        print top = "       |       |       \n"
-        puts "   #{@board[0]}   |   #{@board[1]}   |   #{@board[2]}   "
-        print bottom = "_______|_______|________\n"
-        print top
-        print "   #{@board[3]}   |   #{@board[4]}   |   #{@board[5]}   \n"
-        print bottom
-        print top
-        print "   #{@board[6]}   |   #{@board[7]}   |   #{@board[8]}   \n"
-        print top
+    print top = "       |       |       \n"
+    puts "   #{@board[0]}   |   #{@board[1]}   |   #{@board[2]}   "
+    print bottom = "_______|_______|________\n"
+    print top
+    print "   #{@board[3]}   |   #{@board[4]}   |   #{@board[5]}   \n"
+    print bottom
+    print top
+    print "   #{@board[6]}   |   #{@board[7]}   |   #{@board[8]}   \n"
+    print top
 
     puts 'Choose your desired number'
     move = gets.chomp.to_i
@@ -62,13 +62,17 @@ while playing_game
       move = gets.chomp.to_i
     end
 
+    @game_logic.updt_board(move, 'X') if turns.odd?
 
-    if @game_logic.win?
+    @game_logic.updt_board(move, 'Y') if turns.even?
+
+    if @game_logic.winning_move?
       puts '- - - - - - - - - - - - - - - - - -'
       puts "Game finished, #{current_player}, You Won!"
       puts '- - - - - - - - - - - - - - - - - -'
       puts 'Do you want to replay? Y/N'
       status = gets.chomp.upcase
+      # rubocop: disable Metrics/BlockNesting
       while status != 'Y' && status != 'N'
         puts 'Please type Y or N'
         status = gets.chomp.upcase
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,34 +1,33 @@
 require_relative '../lib/players.rb'
 
 class Logic
-
-attr_reader :board
-def initialize(board)
-  @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @winner = false
-  @win_arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-  @player1_array = Players.player1_array
-  @player2_array = Players.player2_array
-end
-
-def draw?
-  board.count('X') + board.count('O') == 9
-end
-
-def winning_move?(array)
-  win_arr.length.times do |i|
-    return true if win_arr[i].all? { |j| array.include?(j) }
+  attr_reader :board
+  def initialize(_board)
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @winner = false
+    @win_arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    @player1_array = Players.player1_array
+    @player2_array = Players.player2_array
   end
-  false
-end
 
-def updt_board(move, symbol)
-  return false if @board[move - 1] == 'X' || @board[move - 1] == 'O'
+  def draw?
+    board.count('X') + board.count('O') == 9
+  end
 
-  player1_array.push(@board[move - 1]) if symbol ==  'X'
-  player2_array.push(@board[move - 1]) if symbol ==  'Y'
+  def winning_move?(array)
+    win_arr.length.times do |i|
+      return true if win_arr[i].all? { |j| array.include?(j) }
+    end
+    false
+  end
 
-  @board[move - 1] ==  symbol
-end
+  def updt_board(move, symbol)
+    return false if @board[move - 1] == 'X' || @board[move - 1] == 'O'
 
+    player1_array.push(@board[move - 1]) if symbol == 'X'
+
+    player2_array.push(@board[move - 1]) if symbol == 'Y'
+
+    @board[move - 1] == symbol
+  end
 end
